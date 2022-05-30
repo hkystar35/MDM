c446cecf7e079bfd9b77aeb8d0a51d93bebaeb23
hkystar35 <32749858+hkystar35@users.noreply.github.com>
2022-05-29 22:57:41 -0600

    Add credit and comments

--- Apple/Scripts/Set-AzVpnConfig.sh
+++ Apple/Scripts/Set-AzVpnConfig.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-#region Script variables
+#region Variables
+# Main variables
 scriptName="$(/usr/bin/basename "$0")"
 logPipe="/private/tmp/${scriptName%.*}.pipe"
 logPath="/private/var/log/${scriptName%.*}.log"
@@ -8,15 +9,16 @@ consoleUser="$(/usr/bin/stat -f %Su /dev/console)"
 uid=$(id -u "$consoleUser")
 computerName="$(/usr/sbin/scutil --get ComputerName)"
 
-# Exact Name of the Azure VPN Client as seen in macOS, minus the .app
+# Script variables
+# Exact App Name, without .app at the end
 appName="Azure VPN Client"
 appFolderName="${appName}.app"
-# Full Path to the app
 appFullPath="/Applications/${appFolderName}"
 # Full Path to the folder where the XML file needs to be created
 vpnXMLDestinationFolder="/Users/${consoleUser}/Library/Containers/com.microsoft.AzureVpnMac/Data/Library/Application Support/com.microsoft.AzureVpnMac"
-# Name your XML config file
-prodVPNconfigFileName="YourProdVPNName.AzureVpnProfile.xml"
+# VPN Connection Name
+prodVPNname="Contoso_VPN-Profile"
+prodVPNconfigFileName="${prodVPNname}.AzureVpnProfile.xml"
 
 # Paste the contents of your Azure VPN Config file here
 # https://docs.microsoft.com/en-us/azure/vpn-gateway/openvpn-azure-ad-client-mac#download
@@ -27,36 +29,36 @@ prodXmlConfig=$(
 </AzVpnProfile>
 EOFxml
 )
-
-#endregion Script variables
+#endregion Variables
 
 #region Functions
 
 #region Logging functions
+# Credit Brock Walters https://www.linkedin.com/in/brock-walters-247a2990
 /usr/bin/mkfifo "$logPipe"
 /usr/bin/tee -a "$logPath" <"$logPipe" &
 exec &>"$logPipe"
 echo "$(/bin/date "+%Y-%m-%dT%H:%M:%S") [START] logging $scriptName" >>"$logPath"
-echo $( tail -n 1 "$logPath")
+echo $(tail -n 1 "$logPath")
 
 # Logs Info messages
 logInfo() {
-    echo "$(/bin/date "+%Y-%m-%dT%H:%M:%S")  [INFO] $1" >>"$logPath"
-    echo $( tail -n 1 "$logPath")
+  echo "$(/bin/date "+%Y-%m-%dT%H:%M:%S")  [INFO] $1" >>"$logPath"
+  echo $(tail -n 1 "$logPath")
 }
 # Logs Alert messages
 logAlert() {
-    echo "$(/bin/date "+%Y-%m-%dT%H:%M:%S") [ALERT] $1" >>"$logPath"
-    echo $( tail -n 1 "$logPath")
+  echo "$(/bin/date "+%Y-%m-%dT%H:%M:%S") [ALERT] $1" >>"$logPath"
+  echo $(tail -n 1 "$logPath")
 }
 # Exit function to close out FIFO pipe
 exiting() {
-    echo "$(/bin/date "+%Y-%m-%dT%H:%M:%S")  [STOP] logging $scriptName" >>"$logPath"
-	echo $( tail -n 1 "$logPath")
-    /bin/rm -rf "$logPipe"
-    /usr/bin/pkill -ail tee >/dev/null
+  echo "$(/bin/date "+%Y-%m-%dT%H:%M:%S")  [STOP] logging $scriptName" >>"$logPath"
+  echo $(tail -n 1 "$logPath")
+  /bin/rm -rf "$logPipe"
+  /usr/bin/pkill -ail tee >/dev/null
 
-    exit
+  exit
 }
 #endregion Logging functions
 
@@ -65,11 +67,15 @@ exiting() {
 runAsUser() {  
   if [ "$consoleUser" != "loginwindow" ]; then
     logInfo "FUNCTION runAsUser | Console User: $consoleUser | uid: $uid"
+    #logInfo "FUNCTION runAsUser | Trying to launch with launchctl"
     logInfo "FUNCTION runAsUser | Trying to launch with sudo"
-    launchctl asuser "$uid" sudo -u "$currentUser" "$@"
+    #launchctl asuser "$uid"
+    launchctl asuser "$uid" sudo -u "$consoleUser" "$@"
 
   else
     logAlert "no user logged in"
+    # uncomment the exit command
+    # to make the function exit with an error when no user is logged in
     exit 1
   fi
 }
@@ -80,36 +86,50 @@ runAsUser() {
 ## MAIN ##
 ##########
 
-# Start log file
+#region Main Script
+
 logInfo "Computer: $computerName | ConsoleUser: $consoleUser"
 
 # Ensure app is installed first, otherwise exit script
 # In macOS, an App is a Container, not a file, so we use '-d' to check for the directory
 if [ -d "$appFullPath" ]; then
-    logInfo "App installed: $appFolderName"
-    logInfo "Launching $appFolderName.."
-    # Open the app as the logged in user in order to create the destination directory with proper permissions
-    runAsUser open -F -j "$appFullPath"
-    sleep 5
-    logInfo "Killing $appFolderName"
-    # Kill the app
-    pkill -x "$appName"
+  logInfo "App installed: $appFolderName"
+  logInfo "Launching $appFolderName.."
+
+  # Open App as consoleUser then kill. This created the folder we need with required permissions.
+  runAsUser open -F -j "$appFullPath"
+  sleep 5
+  logInfo "Killing $appFolderName"
+  # Kill the app
+  pkill -x "$appName"
 else
-    logAlert "App not installed: $appFolderName"
-    logAlert "Exiting Script"
-    exiting
+  logAlert "App not installed: $appFolderName"
+  logAlert "Exiting Script"
+  exiting
 fi
 
 # Make sure the destination folder was created, otherwise exit script
 if [ -d "$vpnXMLDestinationFolder" ]; then
-    logInfo "folder $vpnXMLDestinationFolder exists"
+  logInfo "folder $vpnXMLDestinationFolder exists"
+  # Remove any existing files or subfolders. This ensures old configs are removed for future updates.
+  rm -rf "$vpnXMLDestinationFolder"/*
 else
-    logAlert "Destination Folder does not exist: $vpnXMLDestinationFolder"
-    exiting
+  logAlert "Destination Folder does not exist: $vpnXMLDestinationFolder"
+  exiting
 fi
 
-# Finally, create the XML config file in the destination
+#region Set Folder Permissions
+# Document folder permissions need in case future app updates break the above process
+#logInfo "Setting permissions of 755 to Folder: $vpnXMLDestinationFolder"
+#chmod 755 "$vpnXMLDestinationFolder"
+
+#logInfo "Setting xattr to Folder: $vpnXMLDestinationFolder"
+#xattr -w com.apple.quarantine "0086;00000000;Azure VPN Client;" "$vpnXMLDestinationFolder"
+#endregion Set Folder Permissions
+
 logInfo "Creating File: $prodVPNconfigFileName"
-cat <<< "$prodXmlConfig" >"${vpnXMLDestinationFolder}/${prodVPNconfigFileName}"
+# Set content for and create file in destination folder
+cat <<<"$prodXmlConfig" >"${vpnXMLDestinationFolder}/${prodVPNconfigFileName}"
 
 exiting
+#endregion Main Script
